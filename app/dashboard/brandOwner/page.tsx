ab83f33c0bc9363f2350fdb8830509097e0aee2a
dawe014 <dawittamiru014@gmail.com>
2025-03-25 15:52:11 -0700

    updated

--- app/dashboard/brandOwner/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function BrandOwnerDashboard() {
-  return <h1 className="text-2xl font-bold">Welcome, Brand Owner!</h1>;
-}
--- app/dashboard/influencer/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function InfluencerDashboard() {
-  return <h1 className="text-2xl font-bold">Welcome, Influencer!</h1>;
-}
